b645093e3cb2678f08d12852f7e8807c11af97cf
skycader <skycader@github.com>
2022-08-25 22:54:31 +0300

    mkdir calculation

--- index.js
+++
@@ -1,6 +0,0 @@
-let num1 = 1*prompt("Enter num1");
-let num2 = 1*prompt("Enter num2");
-
-let output = !isNaN(num1+num2) ? `${num1+num2} ${num1/num2}` : "Некорректный ввод";
-console.log(output);
-document.write("Result: ", output)
