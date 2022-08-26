842f7f4e441336de54050b5e521f68dc51a9a69c
skycader <skycader@github.com>
2022-08-27 00:05:45 +0300

    initializing stack

--- task03/selectFromInterval/selectFromInterval.js
+++
@@ -1,8 +0,0 @@
-//big O here is N (linear), longer input array, longer checking and cutting.
-function selectFromInterval(arr,a,b) {
-	if (isNaN(a+b)) throw new Error("Invalid range")
-	if (arr.filter((item)=>typeof item != 'number').length) throw new Error("Invalid type")
-	return (a<b) ? arr.slice(a-1,b) : arr.slice(b-1,a)
-}
-
-
