93f8dcb077493f08fa73702fa5cff708e7c09935
skycader <skycader@github.com>
2022-08-26 23:42:35 +0300

    added big o

--- task03/selectFromInterval/selectFromInterval.js
+++ task03/selectFromInterval/selectFromInterval.js
@@ -1,3 +1,4 @@
+//big O here is N (linear), longer input array, longer checking and cutting.
 function selectFromInterval(arr,a,b) {
 	if (isNaN(a+b)) throw new Error("Invalid range")
 	if (arr.filter((item)=>typeof item != 'number').length) throw new Error("Invalid type")
