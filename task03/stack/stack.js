44ed0a82c48ec711f6e626af68a18c2861806544
skycader <skycader@github.com>
2022-08-27 01:04:15 +0300

    Stack solved

--- task03/stack/stack.js
+++ task03/stack/stack.js
@@ -1,18 +1,18 @@
 const stack = () => {
 	let stack = []
 	return {
-		add: (item) => { stack.push(item) },
+		push: (item) => { stack.push(item) },
 		peek: () => stack[stack.length-1],
-		remove: () => {stack.pop()},
+		pop: () => {stack.pop()},
 		length: () => stack.length
 	}
 }
 
 let myStack = stack()
-myStack.add(1)
-myStack.add(2)
+myStack.push(1)
+myStack.push(2)
 console.log(myStack.peek())
 console.log(myStack.length())
-myStack.remove()
+myStack.pop()
 console.log(myStack.length())
 
