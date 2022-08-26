825b39532d8fb4e369e346ba40862edd32f127fd
skycader <skycader@github.com>
2022-08-27 01:37:49 +0300

    Solved queue

--- task03/queue/queue.js
+++ task03/queue/queue.js
@@ -1,3 +1,15 @@
+//big O: constant (1)
 const queue = () => {
+	let queue = []
 	return {
+		enqueue: (item) => queue.push(item),
+		dequeue: () => queue.shift(),
+		front: () => queue[0],
+		isEmpty: () => queue.length==0,
+		size: () => queue.length
+	}
+}
+
+
+
 
