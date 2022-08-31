e0086efc051664fdde19549034c63cf8a6de0595
skycader <skycader@github.com>
2022-09-01 00:54:52 +0300

    solved iterator

--- task04/filter/filter.js
+++
@@ -1,9 +0,0 @@
-Array.prototype.myFilter = function(callback,thisArg){
-	const result = []
-	let index = 0
-	for (let item of this) {
-		if (callback.call(thisArg, item,index,this)) result.push(item)
-		index++
-	}
-	return result
-}
