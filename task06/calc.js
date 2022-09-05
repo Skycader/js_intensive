871aa1d91fe9e8544dd559c1972fdb483bac4ec3
skycader <skycader@github.com>
2022-09-05 19:58:38 +0300

    Solved calculator class

---
+++ task06/calc.js
@@ -0,0 +1,26 @@
+class Calculator {
+  constructor(x, y) {
+    if (isNaN(x) || isNaN(y)) throw new Error("Invalid numbers");
+    this._x = x;
+    this._y = y;
+  }
+  setX(x) {
+    this._x = x;
+  }
+  setY(y) {
+    this._y = y;
+  }
+  logSum() {
+    console.log(this._x + this._y);
+  }
+  logMul() {
+    console.log(this._x * this._y);
+  }
+  logSub() {
+    console.log(this._x - this._y);
+  }
+  logDiv() {
+    if (!this._y) throw new Error("Deleting by zero");
+    console.log(this._x / this._y);
+  }
+}
